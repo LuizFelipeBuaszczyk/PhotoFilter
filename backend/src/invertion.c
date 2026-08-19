2d446eed3e7d466f44f8d1a80fbdead22060d1af
LuizFelipeBuaszczyk <163165579+LuizFelipeBuaszczyk@users.noreply.github.com>
2026-08-19 19:44:20 -0300

    fix: correção inverção da imagem

--- backend/src/invertion.c
+++ backend/src/invertion.c
@@ -18,32 +18,27 @@ int invert_horizontal(Image *image, Image *result_image) {
             *result_image->pixels[i * width + width - j - 1].green = *image->pixels[i * width + j].green;
             *result_image->pixels[i * width + width - j - 1].blue = *image->pixels[i * width + j].blue;
             *result_image->pixels[i * width + width - j - 1].alpha = *image->pixels[i * width + j].alpha;
-
-           // Pixel aux = *last_pixel;
-           // *last_pixel = *first_pixel;
-           // *first_pixel = aux;
        }
     }
 
     return 0;
 }
 
-// columns * (rows - i) - 250 + j - 1
 int invert_vertical(Image *image, Image *result_image) {
     int width = image->columns;
     int heigth = image->rows;
 
     for (int i=0; i<image->rows/2; i++){
         for (int j=0; j<image->columns; j++) {
-            *result_image->pixels[i * width + j].red = *image->pixels[width * (heigth - i) - 250 + j - 1].red;
-            *result_image->pixels[i * width + j].green = *image->pixels[width * (heigth - i) - 250 + j - 1].green;
-            *result_image->pixels[i * width + j].blue = *image->pixels[width * (heigth - i) - 250 + j - 1].blue;
-            *result_image->pixels[i * width + j].alpha = *image->pixels[width * (heigth - i) - 250 + j - 1].alpha;
+            *result_image->pixels[i * width + j].red = *image->pixels[width * (heigth - i) - 250 + j].red;
+            *result_image->pixels[i * width + j].green = *image->pixels[width * (heigth - i) - 250 + j].green;
+            *result_image->pixels[i * width + j].blue = *image->pixels[width * (heigth - i) - 250 + j].blue;
+            *result_image->pixels[i * width + j].alpha = *image->pixels[width * (heigth - i) - 250 + j].alpha;
 
-            *result_image->pixels[width * (heigth - i) - 250 + j - 1].red = *image->pixels[i * width + j].red;
-            *result_image->pixels[width * (heigth - i) - 250 + j - 1].green = *image->pixels[i * width + j].green;
-            *result_image->pixels[width * (heigth - i) - 250 + j - 1].blue = *image->pixels[i * width + j].blue;
-            *result_image->pixels[width * (heigth - i) - 250 + j - 1].alpha = *image->pixels[i * width + j].alpha;
+            *result_image->pixels[width * (heigth - i) - 250 + j].red = *image->pixels[i * width + j].red;
+            *result_image->pixels[width * (heigth - i) - 250 + j].green = *image->pixels[i * width + j].green;
+            *result_image->pixels[width * (heigth - i) - 250 + j].blue = *image->pixels[i * width + j].blue;
+            *result_image->pixels[width * (heigth - i) - 250 + j].alpha = *image->pixels[i * width + j].alpha;
         }
     }
 
